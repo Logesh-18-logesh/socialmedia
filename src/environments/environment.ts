9a6aa06d3dcb165d00de4bc84b65e2255896430a
Logesh-18-logesh <logeshdhan@gmail.com>
2024-02-25 16:33:11 +0530

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    appConfigStorageKey: 'PK-social-App@Storage',
+    baseApiUrl: "https://social-media-platform-pk1a8a2s-default-rtdb.firebaseio.com",
+    apiKey: "AIzaSyALQI3ZUKbfV_O2OqU6-EuW3LfL7PNS_lQ",
+    authDomain: "social-media-platform-pk1a8a2s.firebaseapp.com",
+    projectId: "social-media-platform-pk1a8a2s",
+    storageBucket: "social-media-platform-pk1a8a2s.appspot.com",
+    messagingSenderId: "245286146642",
+    appId: "1:245286146642:web:dad98feba20647cdc9458b",
+    measurementId: "G-8V0KKGCJTF"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
